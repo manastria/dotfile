e364fc0f85af2ad64330899e6fc51eebc089781b
jpdemory <jpdemory@manastria.fr>
2020-12-02 18:30:39 +0100

    Add exa alias

---
+++ .shellrc/zshrc.d/02_alias.zsh
@@ -0,0 +1,47 @@
+# -*- mode: shell-script -*-
+
+# DESC: Only pretty_print() the provided string if verbose mode is enabled
+# ARGS: $@ (required): Passed through to pretty_print() function
+# OUTS: None
+function verbose_print() {
+    if [[ -n ${verbose-} ]]; then
+        pretty_print "$@"
+    fi
+}
+
+# DESC: Check a binary exists in the search path
+# ARGS: $1 (required): Name of the binary to test for existence
+#       $2 (optional): Set to any value to treat failure as a fatal error
+# OUTS: None
+function check_binary() {
+    if [[ $# -lt 1 ]]; then
+        >&2 echo 'Missing required argument to check_binary()!' 
+		exit 2
+    fi
+
+    if ! command -v "$1" > /dev/null 2>&1; then
+        if [[ -n ${2-} ]]; then
+            >&2 echo "Missing dependency: Couldn't locate $1." 
+			exit 1
+        else
+            verbose_print "Missing dependency: $1" "${fg_red-}"
+            return 1
+        fi
+    fi
+
+    verbose_print "Found dependency: $1"
+    return 0
+}
+
+if check_binary exa
+then
+  alias l='exa -lbF --git'                                                # list, size, type, git
+  alias ll='exa -lbGF --git'                                             # long list
+  alias llm='exa -lbGd --git --sort=modified'                            # long list, modified date sort
+  alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
+  alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+  
+  # specialty views
+  alias lS='exa -1'                                                              # one column, just names
+  alias lt='exa --tree --level=2'                                         # tree
+fi
