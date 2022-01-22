2a67eedaa14ecc05f23811a6603725f52963edd1
jpdemory <jpdemory@manastria.fr>
2022-01-22 23:33:46 +0100

    Add df alias

--- .shellrc/rc.d/alias.sh
+++ .shellrc/rc.d/alias.sh
@@ -1,3 +1,4 @@
 alias gitscriptexec="find . -regextype posix-egrep -regex \".*\.(sh|zsh)$\" | xargs git update-index --chmod=+x"
 alias getperms="stat -c '%A %a'"
 alias getrealpath="readlink -f "
+alias df="df -x tmpfs -x devtmpfs --human-readable --output=source,fstype,size,used,avail,pcent,itotal,iused,iavail,ipcent"
