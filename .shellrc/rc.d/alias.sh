f137bf135de05140425e1f3e3c49b890ec4a3230
jpdemory <jpdemory@manastria.com>
2022-04-23 23:03:12 +0200

    Add goto

--- .shellrc/rc.d/alias.sh
+++ .shellrc/rc.d/alias.sh
@@ -2,3 +2,4 @@ alias gitscriptexec="find . -regextype posix-egrep -regex \".*\.(sh|zsh)$\" | xa
 alias getperms="stat -c '%A %a'"
 alias getrealpath="readlink -f "
 alias df="df -x tmpfs -x devtmpfs --human-readable --output=source,fstype,size,used,avail,pcent,itotal,iused,iavail,ipcent"
+
---
+++ .shellrc/rc.d/goto.sh
@@ -0,0 +1,4 @@
+# Source goto
+[[ -s "/usr/local/share/goto.sh" ]] && source /usr/local/share/goto.sh
+
+
