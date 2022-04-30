3d3b22a9a8d176e2d749e1ab62e65ae278fb97a4
jpdemory <jpdemory@manastria.fr>
2022-05-01 00:06:58 +0200

    Generalise les scripts

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */root/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/root/.fzf/bin"
+if [[ ! "$PATH" == */${HOME}/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/${HOME}/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/root/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/${HOME}/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/root/.fzf/shell/key-bindings.bash"
+source "/${HOME}/.fzf/shell/key-bindings.bash"
