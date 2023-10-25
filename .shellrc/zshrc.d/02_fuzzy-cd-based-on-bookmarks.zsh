60a6dbcd5579efbf7df8fad19eeda61de2ce145c
jpdemory <jpdemory@manastria.fr>
2023-10-25 22:19:20 +0200

    MAJ de la création de la BDD de fuzzy-cd

--- .shellrc/zshrc.d/02_fuzzy-cd-based-on-bookmarks.zsh
+++ .shellrc/zshrc.d/02_fuzzy-cd-based-on-bookmarks.zsh
@@ -30,22 +30,51 @@ chpwd_functions+=(update_marks)
 # Create database
 ###########################
 
-# Définitions
-MARKS_DIR="$XDG_DATA_HOME/marks"
-DB_FILE="$MARKS_DIR/marks.sqlite"
+#!/bin/bash
 
-# Création du répertoire s'il n'existe pas
-mkdir -p "$MARKS_DIR"
+# Nom personnalisé pour le processus lors de l'envoi des logs à journalctl
+PROCESS_NAME="fuzzy-cd"
 
-# Création de la base de données et de la table si elles n'existent pas
-if [ ! -f "$DB_FILE" ]; then
-	echo "Creating marks database..."
-    sqlite3 "$DB_FILE" << 'INIT'
-    CREATE TABLE marks (
-      dir VARCHAR(200) UNIQUE,
-      weight INTEGER
-    );
-    CREATE INDEX _dir ON marks (dir);
-INIT
+# Envoyer un log indiquant le début de l'exécution du script
+logger -t "$PROCESS_NAME" "Démarrage du script."
+
+# Créer le dossier pour la base de données si nécessaire
+mkdir -p "${XDG_DATA_HOME}/marks"
+
+# Vérifier le succès de la création du dossier
+if [ $? -eq 0 ]; then
+    logger -t "$PROCESS_NAME" "Dossier ${XDG_DATA_HOME}/marks créé ou déjà existant."
+else
+    # Si le dossier n'a pas été créé, envoyer un log d'erreur et quitter le script
+    logger -t "$PROCESS_NAME" "Erreur lors de la création du dossier ${XDG_DATA_HOME}/marks."
+    exit 1
 fi
 
+# Vérifiez si la table "marks" existe déjà dans la base de données
+TABLE_EXISTS=$(sqlite3 "${XDG_DATA_HOME}/marks/marks.sqlite" "SELECT name FROM sqlite_master WHERE type='table' AND name='marks';")
+
+# Si la table "marks" n'existe pas, procédez à sa création
+if [ -z "$TABLE_EXISTS" ]; then
+    sqlite3 "${XDG_DATA_HOME}/marks/marks.sqlite" << 'INIT'
+CREATE TABLE marks (
+  dir VARCHAR(200) UNIQUE,
+  weight INTEGER
+);
+
+CREATE INDEX _dir ON marks (dir);
+INIT
+    # Vérifier le succès de la création de la table
+    if [ $? -eq 0 ]; then
+        logger -t "$PROCESS_NAME" "Table 'marks' créée avec succès."
+    else
+        # Si la table n'a pas été créée, envoyer un log d'erreur et quitter le script
+        logger -t "$PROCESS_NAME" "Erreur lors de la création de la table 'marks'."
+        exit 1
+    fi
+else
+    # Si la table "marks" existe déjà, envoyer un log pour l'indiquer
+    logger -t "$PROCESS_NAME" "La table 'marks' existe déjà."
+fi
+
+# Envoyer un log indiquant la fin de l'exécution du script
+logger -t "$PROCESS_NAME" "Fin du script."
