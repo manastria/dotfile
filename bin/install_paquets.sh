d18bfa5309a5fcd8f7769e3636f3a1da2ee07be6
manastria <manastria@users.noreply.github.com>
2024-08-09 22:40:58 +0200

    chore: update package
    chore: Update install_paquets.sh script to include package update and upgrade steps
    chore: Remove squid-deb-proxy-client package and update package list in install_paquets.sh

--- bin/install_paquets.sh
+++ bin/install_paquets.sh
@@ -5,11 +5,39 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
 NC='\033[0m' # Pas de couleur
 
-# Met à jour la liste des paquets et upgrade les paquets existants
+# Étape 1: Mise à jour de la liste des paquets et mise à niveau des paquets existants
+echo -e "${YELLOW}Étape 1: Mise à jour et mise à niveau des paquets${NC}"
 sudo apt update && sudo apt upgrade -y
+echo -e "${GREEN}Mise à jour et mise à niveau terminées.${NC}"
 
+# Étape 2: Désinstallation des paquets obsolètes
+echo -e "${YELLOW}Étape 2: Désinstallation des paquets obsolètes${NC}"
+# Liste des paquets à supprimer
+packages_to_remove=(squid-deb-proxy-client)
+to_remove=()
+
+for pkg in "${packages_to_remove[@]}"; do
+    if dpkg -s $pkg &> /dev/null; then
+        echo -e "${RED}Le paquet $pkg sera désinstallé.${NC}"
+        to_remove+=($pkg)
+    else
+        echo -e "${BLUE}Le paquet $pkg n'est pas installé.${NC}"
+    fi
+done
+
+if [ ${#to_remove[@]} -gt 0 ]; then
+    echo -e "${YELLOW}Désinstallation des paquets: ${to_remove[*]}${NC}"
+    sudo apt remove -y "${to_remove[@]}"
+    echo -e "${GREEN}Désinstallation terminée.${NC}"
+else
+    echo -e "${GREEN}Aucun paquet à désinstaller.${NC}"
+fi
+
+# Étape 3: Installation des nouveaux paquets
+echo -e "${YELLOW}Étape 3: Installation des nouveaux paquets${NC}"
 # Liste des paquets à installer
 packages=(vim bat fd-find zsh fzf sqlite3 git curl wget htop tree ncdu aptitude tmux haveged openssh-server auto-apt-proxy)
 # Liste des paquets à installer pour les addons de VirtualBox
@@ -42,8 +70,11 @@ done
 
 # Installe tous les paquets en une seule commande
 if [ ${#to_install[@]} -gt 0 ]; then
-    echo "Installation des paquets: ${to_install[*]}"
+    echo -e "${YELLOW}Installation des paquets: ${to_install[*]}${NC}"
     sudo apt install -y "${to_install[@]}"
+    echo -e "${GREEN}Installation des paquets terminée.${NC}"
 else
-    echo "Aucun nouveau paquet à installer."
+    echo -e "${GREEN}Aucun nouveau paquet à installer.${NC}"
 fi
+
+echo -e "${YELLOW}Script terminé.${NC}"
