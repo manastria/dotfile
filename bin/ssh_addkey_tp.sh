8a01feb37fa8e4416b09c87422cf61f776851ca8
manastria <manastria@users.noreply.github.com>
2024-10-31 00:18:08 +0100

    Refactor ssh_addkey_tp.sh for improved readability and maintainability

--- bin/ssh_addkey_tp.sh
+++ bin/ssh_addkey_tp.sh
@@ -24,7 +24,7 @@
 #  - 1.0: Version initiale.
 #  - 1.1: Ajout de la vérification des doublons et amélioration des messages de journalisation.
 #
-#  CONTACT: 
+#  CONTACT:
 ###############################################################
 
 
@@ -34,50 +34,50 @@ set -e
 # Variables pour les clés SSH
 KEY1="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDc+e2L7GFcoWgE2qhVpQmBq2jiCZtXj1vIpFG/+N7Yw TP04"
 
-# Fonction pour ajouter une clé SSH si elle n'existe pas déjà
+# Fonction pour ajouter une clé SSH si elle n'existe pas déj�
 add_ssh_key() {
-  KEY=$1
-  if grep -qF "$KEY" ~/.ssh/authorized_keys; then
-    echo "La clé SSH est déjà présente : $KEY"
-  else
-    echo "$KEY" >> ~/.ssh/authorized_keys
-    echo "Clé SSH ajoutée : $KEY"
-  fi
+    KEY=$1
+    if grep -qF "$KEY" ~/.ssh/authorized_keys; then
+        echo "La clé SSH est déjà présente : $KEY"
+    else
+        echo "$KEY" >> ~/.ssh/authorized_keys
+        echo "Clé SSH ajoutée : $KEY"
+    fi
 }
 
 # Crée le répertoire .ssh s'il n'existe pas
 setup_ssh_directory() {
-  mkdir -p ~/.ssh
-  touch ~/.ssh/authorized_keys
-  echo "Répertoire .ssh et fichier authorized_keys créés ou déjà existants."
+    mkdir -p ~/.ssh
+    touch ~/.ssh/authorized_keys
+    echo "Répertoire .ssh et fichier authorized_keys créés ou déjà existants."
 }
 
 # Remplace authorized_keys s'il est un lien symbolique et qu'un fichier authorized_keys2 existe
 replace_symlink() {
-  # Vérifie si authorized_keys est un lien symbolique (-L) et si authorized_keys2 est un fichier régulier (-f).
-  # Si ces deux conditions sont vraies, le lien symbolique est remplacé par authorized_keys2.
-  if [ -L ~/.ssh/authorized_keys -a -f ~/.ssh/authorized_keys2 ]; then
-    rm ~/.ssh/authorized_keys
-    mv ~/.ssh/authorized_keys2 ~/.ssh/authorized_keys
-    echo "Lien symbolique authorized_keys remplacé par le fichier authorized_keys2."
-  fi
+    # Vérifie si authorized_keys est un lien symbolique (-L) et si authorized_keys2 est un fichier régulier (-f).
+    # Si ces deux conditions sont vraies, le lien symbolique est remplacé par authorized_keys2.
+    if [ -L ~/.ssh/authorized_keys -a -f ~/.ssh/authorized_keys2 ]; then
+        rm ~/.ssh/authorized_keys
+        mv ~/.ssh/authorized_keys2 ~/.ssh/authorized_keys
+        echo "Lien symbolique authorized_keys remplacé par le fichier authorized_keys2."
+    fi
 }
 
 # Crée un lien symbolique vers authorized_keys
 create_symlink() {
-  # Supprime authorized_keys2 s'il existe (fichier ou lien symbolique)
-  if [ -e ~/.ssh/authorized_keys2 ]; then
-    rm ~/.ssh/authorized_keys2
-  fi
-  ln -sf ~/.ssh/authorized_keys ~/.ssh/authorized_keys2
-  echo "Lien symbolique ~/.ssh/authorized_keys2 créé ou mis à jour."
+    # Supprime authorized_keys2 s'il existe (fichier ou lien symbolique)
+    if [ -e ~/.ssh/authorized_keys2 ]; then
+        rm ~/.ssh/authorized_keys2
+    fi
+    ln -sf ~/.ssh/authorized_keys ~/.ssh/authorized_keys2
+    echo "Lien symbolique ~/.ssh/authorized_keys2 créé ou mis à jour."
 }
 
 # Définit les permissions
 set_permissions() {
-  chmod -R 700 ~/.ssh
-  chmod 0600 ~/.ssh/authorized_keys*
-  echo "Permissions définies pour le répertoire .ssh et les fichiers authorized_keys."
+    chmod -R 700 ~/.ssh
+    chmod 0600 ~/.ssh/authorized_keys*
+    echo "Permissions définies pour le répertoire .ssh et les fichiers authorized_keys."
 }
 
 # Exécution des fonctions
