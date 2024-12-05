63787dcde239a83aaa474f8f7a117398a64521bc
manastria <manastria@users.noreply.github.com>
2024-12-05 18:32:14 +0100

    Refactor history management in .bashrc and history.bash for improved performance and usability

--- .bashrc
+++ .bashrc
@@ -33,18 +33,6 @@ if [ -d "$HOME"/.shellrc/rc.d ]; then
   done
 fi
 
-# append to the history file, don't overwrite it
-shopt -s histappend
-
-# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=1000
-HISTFILESIZE=2000
-# don't put duplicate lines or lines starting with space in the history.
-# See bash(1) for more options
-HISTCONTROL=ignoreboth
-
-
-
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
 shopt -s checkwinsize
--- .shellrc/bashrc.d/history.bash
+++ .shellrc/bashrc.d/history.bash
@@ -1,18 +1,11 @@
 # -*- mode: bash -*-
 
-# don't put duplicate lines in the history. See bash(1) for more options
-export HISTCONTROL=ignoredups
-
-# don't put duplicate lines or lines starting with space in the history.
-# See bash(1) for more options
-HISTCONTROL=ignoreboth
-
-# append to the history file, don't overwrite it
-shopt -s histappend
-
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=1000
-HISTFILESIZE=2000
+HISTSIZE=10000
+HISTFILESIZE=20000
 
 # activation date_heure dans la commande history
 export HISTTIMEFORMAT="%Y/%m/%d_%T : "
+
+shopt -s histappend 
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
