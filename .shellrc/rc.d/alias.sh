77e2dbf404e8f497a6a2289669aca5607a17a392
manastria <manastria@users.noreply.github.com>
2024-12-29 18:47:24 +0100

    Refactor alias section in alias.sh for improved organization and clarity

--- .shellrc/rc.d/alias.sh
+++ .shellrc/rc.d/alias.sh
@@ -1,17 +1,66 @@
+################################################################################
+# +--------------------------------------------------------------------------+ #
+# +                                                                          + #
+# +                                SECTION 01                                + #
+# +                                                                          + #
+# +--------------------------------------------------------------------------+ #
+################################################################################
+
+alias grepcolor='grep --color=always'
+
+################################################################################
+# +--------------------------------------------------------------------------+ #
+# +                                                                          + #
+# +                                SECTION 01                                + #
+# +                                                                          + #
+# +--------------------------------------------------------------------------+ #
+################################################################################
+
 alias gitscriptexec="find . -regextype posix-egrep -regex \".*\.(sh|zsh)$\" | xargs git update-index --chmod=+x"
+
+################################################################################
+# +--------------------------------------------------------------------------+ #
+# +                                                                          + #
+# +                                SECTION 01                                + #
+# +                                                                          + #
+# +--------------------------------------------------------------------------+ #
+################################################################################
 alias getperms="stat -c '%A %a %U %G'"
 alias getrealpath="readlink -f "
 alias df="df -x tmpfs -x devtmpfs --human-readable --output=source,fstype,size,used,avail,pcent,itotal,iused,iavail,ipcent"
 alias ls="ls --color=auto --classify --tabsize=0 --group-directories-first -v" # --literal --color=auto --show-control-chars --human-readable --group-directories-first'
+alias backup_permissions="stat -c '%a %U %G' fichier_cible > permissions_owner_group.txt"
+alias restore_permissions="read chmod_value owner group && chmod $chmod_value fichier_cible && chown $owner:$group fichier_cible"
+
+################################################################################
+# +--------------------------------------------------------------------------+ #
+# +                                                                          + #
+# +                                SECTION 01                                + #
+# +                                                                          + #
+# +--------------------------------------------------------------------------+ #
+################################################################################
 
 alias fzfon='[ -f ~/.fzf.bash ] && source ~/.fzf.bash'
 alias fasdon='eval "$(fasd --init auto)"'
 
-alias backup_permissions="stat -c '%a %U %G' fichier_cible > permissions_owner_group.txt"
-alias restore_permissions="read chmod_value owner group && chmod $chmod_value fichier_cible && chown $owner:$group fichier_cible"
+################################################################################
+# +--------------------------------------------------------------------------+ #
+# +                                                                          + #
+# +                                SECTION 01                                + #
+# +                                                                          + #
+# +--------------------------------------------------------------------------+ #
+################################################################################
+
 
 alias nm-con-status='nmcli -f NAME,TYPE,DEVICE,STATE con show' # --active
 
+################################################################################
+# +--------------------------------------------------------------------------+ #
+# +                                                                          + #
+# +                                SECTION 01                                + #
+# +                                                                          + #
+# +--------------------------------------------------------------------------+ #
+################################################################################
 
 
 alias pgp_export_public='function gpg_export() { gpg --export --export-options no-export-minimal,no-export-clean -a -o "$1.asc" "$1"; }; gpg_export'
@@ -19,7 +68,17 @@ alias pgp_publish_openpgp='function pgp_publish_openpgp() { gpg --export "$1" | 
 alias pgp_export_private='function gpg_export_private() { gpg --armor --export-secret-keys --export-options export-backup --output "$1-private.asc" "$1"; }; gpg_export_private'
 alias pgp_import_private='function gpg_import_private() { gpg --import-options restore,keep-ownertrust --import "$1-private.asc"; }; gpg_import_private'
 
-alias grepcolor='grep --color=always'
+
+
+
+################################################################################
+# +--------------------------------------------------------------------------+ #
+# +                                                                          + #
+# +                                SECTION 01                                + #
+# +                                                                          + #
+# +--------------------------------------------------------------------------+ #
+################################################################################
+
 
 # Affiche les processus avec les colonnes essentielles pour une analyse rapide :
 #   - PPID : PID du processus parent.
