dfccf1ed0140178f07b8fe863ca801af2a46ac14
manastria <manastria@users.noreply.github.com>
2026-01-12 12:37:43 +0100

    Ajout de nouveaux paquets à installer dans le script install_paquets.sh

--- bin/install_paquets.sh
+++ bin/install_paquets.sh
@@ -58,7 +58,10 @@ echo -e "${YELLOW}Étape 3: Installation des nouveaux paquets${NC}"
 packages=(
     aptitude
     auto-apt-proxy
+    bash-completion 
     bat
+    byobu
+    ccze
     curl
     direnv
     fd-find
@@ -69,15 +72,26 @@ packages=(
     haveged
     htop
     jq
+    lnav
     lsd
+    mlocate
+    most
+    multitail
     ncdu
     openssh-server
     python3-rich
+    reptyr
+    screen
+    screenfetch
     sqlite3
+    sudo
     tmux
     tree
+    unrar
+    unzip
     vim
     wget
+    zip
     zsh
 )
 packages+=(
