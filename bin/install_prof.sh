c0f8701f7858485edf2165f6783b9d77943ffbb2
manastria <manastria@users.noreply.github.com>
2026-02-07 05:01:47 +0100

    Fix privilege escalation by changing 'su' to 'sudo su' in install_prof.sh

--- bin/install_prof.sh
+++ bin/install_prof.sh
@@ -39,7 +39,7 @@ if [ "$USER" != "prof" ]; then
     echo -e "${YELLOW}Bascule vers session prof...${NC}"
     PROXY_ENV=""
     [ -n "$http_proxy" ] && PROXY_ENV="export http_proxy=$http_proxy https_proxy=$https_proxy;"
-    su - prof -c "$PROXY_ENV curl -fsSL https://raw.githubusercontent.com/manastria/dotfile/refs/heads/dev1/bin/install_prof.sh | bash"
+    sudo su - prof -c "$PROXY_ENV curl -fsSL https://raw.githubusercontent.com/manastria/dotfile/refs/heads/dev1/bin/install_prof.sh | bash"
     
 else
     # Phase 2 : installer la config yadm
