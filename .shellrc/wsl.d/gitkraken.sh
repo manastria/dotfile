943d719f2bc9ffd81236f5cf8ae150eb41a8ae51
manastria <manastria@users.noreply.github.com>
2026-03-28 17:45:28 +0100

    feat: add logic to automatically find and set the latest GitKraken installation path

--- .shellrc/wsl.d/gitkraken.sh
+++ .shellrc/wsl.d/gitkraken.sh
@@ -1,3 +1,9 @@
+
+
+# Trouve automatiquement la dernière version installée
+GITKRAKEN_DIR=$(find /mnt/c/Users/jpdem/AppData/Local/gitkraken -maxdepth 1 -name "app-*" -type d 2>/dev/null | sort -V | tail -1)
+[ -n "$GITKRAKEN_DIR" ] && export PATH="$PATH:$GITKRAKEN_DIR"
+
 # Fonction plutôt qu'alias, pour passer le chemin converti
 gk() {
   local winpath
